3a2f23f8fd32a081b8d4ea62109af1153f139858
Taevas <isterix.bowteam@gmail.com>
2020-11-19 18:48:09 +0100

    Menu updated, buttons to go from lessons to lessons updated The lesson you are currently reading is now highlighted on the menu Menu now doesn't show up at the beginning of the loading process "other-lessons" buttons' text is now automatized for devs' convenience (EN) Default text for those buttons added

--- menu.js
+++ menu.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+	//Load the menu
 	$("#menu_content").hide()
 	$("#menu_toggle").click(function() {
 		$("#menu_content").toggle(200)
@@ -6,4 +8,23 @@ $(document).ready(function() {
 	$("#menu_content").click(function() {
 		$("#menu_content").toggle(200)
 	})
+
 })
+
+function changeButtons() {
+
+	var title = document.title //Title represents current lesson
+	var prevButton = $("#prev-lesson")
+	var nextButton = $("#next-lesson")
+	var lists = $("#menu_content").find("li") 
+	lists.each(function(i) {
+		console.log($(lists[i]).text())
+		if ($(this).text() == title) {
+			$(this).css("backgroundColor", "rgb(180, 105, 255)") //Highlight in the menu the current lesson
+			//vv Index is used because prev()/next() do not work inter-sub-categories vv
+			if (prevButton.length) {prevButton.text("<- " + $(lists[i-1]).text())}
+			if (nextButton.length) {nextButton.text($(lists[i+1]).text() + " ->")}
+		}
+	})
+
+}
