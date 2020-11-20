b27f6579cda4132ed34e42fb50677c471badedbf
Taevas <isterix.bowteam@gmail.com>
2020-11-20 02:43:23 +0100

    Buttons updated, menu added to French lessons, root page fixed
    "other-lessons" buttons' links are now automatized for devs' convenience
    (FR) Menu available in the French version of CyberPop!
    Language selection screen's list is now actually a proper list

--- menu.js
+++ menu.js
@@ -18,12 +18,17 @@ function changeButtons() {
 	var nextButton = $("#next-lesson")
 	var lists = $("#menu_content").find("li") 
 	lists.each(function(i) {
-		console.log($(lists[i]).text())
 		if ($(this).text() == title) {
 			$(this).css("backgroundColor", "rgb(180, 105, 255)") //Highlight in the menu the current lesson
 			//vv Index is used because prev()/next() do not work inter-sub-categories vv
-			if (prevButton.length) {prevButton.text("<- " + $(lists[i-1]).text())}
-			if (nextButton.length) {nextButton.text($(lists[i+1]).text() + " ->")}
+			if (prevButton.length) {
+				prevButton.text("<- " + $(lists[i-1]).text())
+				prevButton.attr("href", $(lists[i-1]).find("a").attr("href"))
+			}
+			if (nextButton.length) {
+				nextButton.text($(lists[i+1]).text() + " ->")
+				nextButton.attr("href", $(lists[i+1]).find("a").attr("href"))
+			}
 		}
 	})
 
