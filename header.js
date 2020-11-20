71bf9ecba15438e4a091b6efdf7c62da031cad08
Taevas <isterix.bowteam@gmail.com>
2020-11-20 19:56:33 +0100

    Attempt at fixing "Client-side URL redirect" security issue
    mfw i gotta worry about phising when nobody uses my "product"

--- header.js
+++ header.js
@@ -1,7 +1,12 @@
 function languageSwitch(selection) {
 	var val = selection.options[selection.selectedIndex].value;
-	var url = window.location.href.split("/");
-	url[3] = val;
-	url = url.join("/")
-	window.location.replace(url)
+	if (val == "english" || val == "francais" || val == "deutsche") {
+		if (val == "english") {lang = "en"}
+		if (val == "francais") {lang = "fr"}
+		if (val == "deutsche") {lang = "de"}
+		var url = window.location.href.split("/");
+		url[3] = lang;
+		url = url.join("/")
+		window.location.replace(url)
+	} else {console.log("Invalid value given!")}
 }
