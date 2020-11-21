c8f4a438fab63e2f00cac49313df91bde351cd73
Taevas <isterix.bowteam@gmail.com>
2020-11-21 01:02:04 +0100

    Fix language menu not working properly on GitHub Pages

--- header.js
+++ header.js
@@ -1,9 +1,10 @@
 function languageSwitch() {
 	var url = window.location.href.split("/")
+	var i = url[2].includes("github.io") ? 4 : 3
 	$(".lang-select").children().each(function() {
-		if ($(this).text() == "English") {url[3] = "en"}
-		if ($(this).text() == "Français") {url[3] = "fr"}
-		if ($(this).text() == "Deutsche") {url[3] = "de"}
+		if ($(this).text() == "English") {url[i] = "en"}
+		if ($(this).text() == "Français") {url[i] = "fr"}
+		if ($(this).text() == "Deutsche") {url[i] = "de"}
 		
 		$(this).attr("href", url.join("/"))
 	})
